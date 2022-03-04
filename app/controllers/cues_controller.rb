d692f36ba8e865e2e372dd1033fdb3be66f9f6e5
stephan ye <ye.stephan@gmail.com>
2022-03-04 11:35:52 +0100

    dynamic message on cues index page

--- app/controllers/cues_controller.rb
+++ app/controllers/cues_controller.rb
@@ -1,9 +1,13 @@
 class CuesController < ApplicationController
   before_action :authenticate_user!
   def index
+    origin = params[:url_origin]
     @cues = Cue.all
     # If first time then pass this string: "Let's create your first cue"
-    @first_time_msg = "Welcome 🎉 Let's setup your first Savecue!"
-    @normal_msg = "Welcome 🎉 Let's setup your first Savecue!"
+    if origin == "signup"
+      @msg = "Welcome 🎉 Let's setup your first Savecue!"
+    else
+      @msg = "Choose a cue"
+    end
   end
 end
