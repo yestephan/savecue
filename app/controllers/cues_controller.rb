85efb9619ef4435aa06085d23ce52a7dc04efdc5
stephan ye <ye.stephan@gmail.com>
2022-03-04 07:44:36 +0100

    redirection almost wokring

--- app/controllers/cues_controller.rb
+++ app/controllers/cues_controller.rb
@@ -2,5 +2,6 @@ class CuesController < ApplicationController
   before_action :authenticate_user!
   def index
     @cues = Cue.all
+    # If first time then pass this string: "Let's create your first cue"
   end
 end
