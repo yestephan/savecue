a9cc2d9767d35eff21c687aad00e21d2fbbf513b
Mahtab Mahtdavifar <mhtbmahdavifar@gmail.com>
2022-03-07 14:23:38 +0100

    Updated the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home, :raise => false
   def home
-    @total_saved = 15
   end
 
   def ui_kit
   end
-
 end
